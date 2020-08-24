fd5db2fb3081477db3ca0f30c8da4fa457b6a94d
Drew Thompson <drewallynthompson@gmail.com>
2020-08-24 22:28:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -49,7 +49,7 @@ def draw?(board)
 end
 
 def over(board)
-  if won?(board) == true || full?(board) == true
+  if won?(board) == true || full?(board) == true || draw?(board) == true
     return true
   else
     false
