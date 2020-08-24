d0d855200031293a02c948aa21801db06e0f1890
Drew Thompson <drewallynthompson@gmail.com>
2020-08-24 21:14:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,7 +25,7 @@ def won?(board)
     state_2 = board[windex_2]
     state_2 = board[windex_2]
     
-    position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+    state_1 == state_2 && state_2 == state_3 && position_taken?(board, windex_1)
     return combos
   end
 end
